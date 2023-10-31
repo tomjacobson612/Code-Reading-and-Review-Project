2217c56f9bb264867d9701c42d55291ad854f855
Tom Jacobson <tjacobson612@gmail.com>
2023-10-30 17:23:31 -0700

    Naming pass over Game class.

--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -39,23 +39,21 @@ restart_button = Button(600, 1040, restart_img)
 
 class Game:
     def __init__(self):
-        self.running = True
-        self.solved = False
+        self.gameRunning = True
+        self.boardSolved = False
 
-        #Colors
-        self.og_grid_color = (0, 0, 0)
-        self.solved_color = (136, 8, 8)
+        self.cellColor = (0, 0, 0)
+        self.solutionColor = (136, 8, 8)
 
-        #Board
-        # self.response = requests.get('https://sugoku.herokuapp.com/board?difficulty=easy')
         self.board = self.generate_board()
-        self.board_unchanging = self.generate_board()
+        self.unchangingBoard = self.generate_board()
 
     def on_init(self):
         pygame.init()
-        self.running = True
+        self.gameRunning = True
 
     def generate_board(self) -> list[list[int]]:
+        # self.response = requests.get('https://sugoku.herokuapp.com/board?difficulty=easy')
         default_grid = [
         [5, 3, 0, 0, 7, 0, 0, 0, 0],
         [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -68,62 +66,58 @@ class Game:
         [0, 0, 0, 0, 8, 0, 0, 7, 9] ]
         return default_grid
 
-    def on_event(self, event):
-        if event.type == pygame.QUIT:
-            self.running = False
-
-    def draw_grid(self):
+    def drawGrid(self, color, borderWidth, innerLineWidth):
         pygame.init()
         displayWindow.fill(backgroundColor)
 
         for i in range(0, 10):
             if i % 3 == 0:
-                pygame.draw.line(displayWindow, (0, 0, 0), (100 + 100 * i, 100), (100 + 100 * i, 1000), 4)
-                pygame.draw.line(displayWindow, (0, 0, 0), (100, 100 + 100 * i), (1000, 100 + 100 * i), 4)
+                pygame.draw.line(displayWindow, color, (100 + 100 * i, 100), (100 + 100 * i, 1000), borderWidth)
+                pygame.draw.line(displayWindow, color, (100, 100 + 100 * i), (1000, 100 + 100 * i), borderWidth)
 
-            pygame.draw.line(displayWindow, (0, 0, 0), (100 + 100 * i, 100), (100 + 100 * i, 1000), 1)
-            pygame.draw.line(displayWindow, (0, 0, 0), (100, 100 + 100 * i), (1000, 100 + 100 * i), 1)
+            pygame.draw.line(displayWindow, color, (100 + 100 * i, 100), (100 + 100 * i, 1000), innerLineWidth)
+            pygame.draw.line(displayWindow, color, (100, 100 + 100 * i), (1000, 100 + 100 * i), innerLineWidth)
 
         pygame.display.update()
 
-    def populate_grid(self):
+    def initializeBoard(self):
         font = pygame.font.SysFont('Arial', 70)
         for i in range(len(self.board[0])):
             for j in range(len(self.board[0])):
                 if 0 < self.board[i][j] < 10:
-                    value = font.render(str(self.board[i][j]), True, self.og_grid_color)
-                    displayWindow.blit(value, ((j + 1) * 100 + 30, (i + 1) * 100 + 15))
+                    cellValue = font.render(str(self.board[i][j]), True, self.cellColor)
+                    displayWindow.blit(cellValue, ((j + 1) * 100 + 30, (i + 1) * 100 + 15))
         pygame.display.update()
 
-    def check_board(self):
+    def checkBoard(self):
         # check rows:
-        for i in range(len(self.board)):
-            seen = []
-            for j in range(len(self.board[0])):
-                if self.board[i][j] not in seen and self.board[i][j] != 0:
-                    seen.append(self.board[i][j])
-                elif self.board[i][j] != 0 and self.board[i][j] in seen:
+        for row in range(len(self.board)):
+            checked = []
+            for column in range(len(self.board[0])):
+                if self.board[row][column] not in checked and self.board[row][column] != 0:
+                    checked.append(self.board[row][column])
+                elif self.board[row][column] != 0 and self.board[row][column] in checked:
                     return False
 
         # check columns:
-        for i in range(len(self.board[0])):
-            seen = []
-            for j in range(len(self.board)):
-                if self.board[j][i] not in seen and self.board[j][i] != 0:
-                    seen.append(self.board[j][i])
-                elif self.board[j][i] != 0 and self.board[j][i] in seen:
+        for row in range(len(self.board[0])):
+            checked = []
+            for column in range(len(self.board)):
+                if self.board[column][row] not in checked and self.board[column][row] != 0:
+                    checked.append(self.board[column][row])
+                elif self.board[column][row] != 0 and self.board[column][row] in checked:
                     return False
 
         # check each 3x3
         row = 0
         column = 0
         while row < 9 and column < 9:
-            seen = []
-            for i in range(row, row + 3):
-                for j in range(column, column + 3):
-                    if self.board[i][j] not in seen and self.board[i][j] != 0:
-                        seen.append(self.board[i][j])
-                    elif self.board[i][j] != 0 and self.board[i][j] in seen:
+            checked = []
+            for row in range(row, row + 3):
+                for column in range(column, column + 3):
+                    if self.board[row][column] not in checked and self.board[row][column] != 0:
+                        checked.append(self.board[row][column])
+                    elif self.board[row][column] != 0 and self.board[row][column] in checked:
                         return False
             if column < 9:
                 column += 3
@@ -133,50 +127,50 @@ class Game:
 
         return True
 
-    def find_next_empty(self):
-        for i in range(len(self.board)):
-            for j in range(len(self.board[0])):
-                if self.board[i][j] == 0:
-                    return (i, j)  # row and column
+    def nextEmptyCell(self):
+        for row in range(len(self.board)):
+            for column in range(len(self.board[0])):
+                if self.board[row][column] == 0:
+                    return (row, column)
         return None
 
-    def solve(self):
-        found = self.find_next_empty()
+    def solveBoard(self):
+        emptyCell = self.nextEmptyCell()
 
-        if not found:  # No empty spaces left
+        if not emptyCell:
             return True
         else:
             for i in range(1, 10):
-                self.board[found[0]][found[1]] = i
-                if self.check_board():
-                    if self.solve():
-                        self.solved = True
+                self.board[emptyCell[0]][emptyCell[1]] = i
+                if self.checkBoard():
+                    if self.solveBoard():
+                        self.boardSolved = True
                         return self.board
-                self.board[found[0]][found[1]] = 0
+                self.board[emptyCell[0]][emptyCell[1]] = 0
         return False
 
-    def populate_solved(self):
+    def populateSolution(self):
         font = pygame.font.SysFont('Arial', 70)
-        for i in range(len(self.board[0])):
-            for j in range(len(self.board[0])):
-                if 0 < self.board[i][j] < 10 and self.board[i][j] != self.board_unchanging[i][j]:
-                    value = font.render(str(self.board[i][j]), True, self.solved_color)
-                    displayWindow.blit(value, ((j + 1) * 100 + 30, (i + 1) * 100 + 15))
+        for row in range(len(self.board[0])):
+            for column in range(len(self.board[0])):
+                if 0 < self.board[row][column] < 10 and self.board[row][column] != self.unchangingBoard[row][column]:
+                    cellValue = font.render(str(self.board[row][column]), True, self.solutionColor)
+                    displayWindow.blit(cellValue, ((column + 1) * 100 + 30, (row + 1) * 100 + 15))
         pygame.display.update()
 
 def main():
     app = Game()
-    app.draw_grid()
-    app.populate_grid()
+    app.drawGrid((0, 0, 0), 4, 1)
+    app.initializeBoard()
 
     while True:
         if restart_button.draw():
             app = Game()
-            app.draw_grid()
-            app.populate_grid()
-        if solve_button.draw() and not app.solved:
-            app.solve()
-            app.populate_solved()
+            app.drawGrid((0, 0, 0), 4, 1)
+            app.initializeBoard()
+        if solve_button.draw() and not app.boardSolved:
+            app.solveBoard()
+            app.populateSolution()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
