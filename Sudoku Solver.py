caadd7bc7ec9c021de82da60bc83e26df081cc99
Tom Jacobson <tjacobson612@gmail.com>
2023-11-02 14:34:05 -0700

    added caption data member to window class

--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -1,13 +1,15 @@
 import pygame
 
 class Window():
-    def __init__(self, height, width, backgroundColor):
+    def __init__(self, height, width, backgroundColor, caption):
         self.height = height
         self.width = width
         self.backgroundColor = backgroundColor
+        self.caption = caption
         self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption(self.caption)
 
-displayWindow = Window(1200, 1100, (251, 247, 245))
+displayWindow = Window(1200, 1100, (251, 247, 245), 'Sudoku Solver')
 
 class Button():
     def __init__(self, x, y, icon):
